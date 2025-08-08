35d6acb4977f7b95ec96a835f74225a5d4d21d59
mindombant <mindombemarc@gmail.com>
2025-08-08 21:20:45 +0100

    ajustage position icon

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -47,7 +47,7 @@ if (formData.password.length < 6) return toast.error("Le mot de passe doit conte
   };
 
   return (
-    <div className="min-h-screen grid lg:grid-cols-2">
+    <div className="min-h-screen grid lg:grid-cols-2 mt-12">
       {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
